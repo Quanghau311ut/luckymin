1a0c27ebfcfb1008c791714860e80757bc98030f
Quanghau311ut <107594919+Quanghau311ut@users.noreply.github.com>
2022-06-16 08:54:29 +0700

    upload

---
+++ js/Scriptdetail.js
@@ -0,0 +1,43 @@
+
+var i=4
+var N=13
+function detail4() {
+    i = 4;
+    document.getElementById("future").setAttribute("src", i + ".jpg")
+}
+function detail5() {
+    i =5;
+    document.getElementById("future").setAttribute("src", i + ".jpg")
+}
+function detail6() {
+    i = 6;
+    document.getElementById("future").setAttribute("src", i + ".jpg")
+}
+function detail7() {
+    i = 7;
+    document.getElementById("future").setAttribute("src", i + ".jpg")
+}
+function detail8() {
+    i = 8;
+    document.getElementById("future").setAttribute("src", i + ".jpg")
+}
+function detail9() {
+    i = 9;
+    document.getElementById("future").setAttribute("src", i + ".jpg")
+}
+function detail10() {
+    i = 10;
+    document.getElementById("future").setAttribute("src", i + ".jpg")
+}
+function detail11() {
+    i = 11;
+    document.getElementById("future").setAttribute("src", i + ".jpg")
+}
+function detail12() {
+    i = 12;
+    document.getElementById("future").setAttribute("src", i + ".jpg")
+}
+function detail13() {
+    i = 13;
+    document.getElementById("future").setAttribute("src", i + ".jpg")
+}
